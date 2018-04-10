f5e94e08cdd3a1b4d6b64307f3701abd7f3b98da
edwardWang95 <edward.wang.95@gmail.com>
2018-04-10 16:57:36 -0400

    AES decryption is 128 bits, or 16 byte blocks at a time. Example test decrypt is a total of 58 bytes, therefore brocken into 4 byte arrays with the last one added with padding to the last 8 bytes with 0's

--- app/src/main/java/edwardwang/myapplication/MainActivity.kt
+++ app/src/main/java/edwardwang/myapplication/MainActivity.kt
@@ -11,13 +11,14 @@ import java.net.InetAddress
 import java.net.Socket
 import java.security.spec.AlgorithmParameterSpec
 import javax.crypto.Cipher
+import javax.crypto.IllegalBlockSizeException
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 class MainActivity : AppCompatActivity() {
 
     var isConnected:Boolean = false
-    var testFileKey:ByteArray = "000102030405060708090A0B0C0D0E0F".toByteArray()
+    //val testFileKey = "000102030405060708090A0B0C0D0E0F".toByteArray()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,23 +51,37 @@ class MainActivity : AppCompatActivity() {
         try
         {
             val serverOutputTextView:TextView = findViewById<TextView>(R.id.serverOutput)
-
-
+            //get key
             val inputStream = resources.openRawResource(R.raw.test_aes)
             val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-
+            val testFileKey = ByteArray(16)
+            for(i in 0..15)
+            {
+                testFileKey[i] = i.toByte()
+            }
             var lineStr: String = bufferedReader.readLine()
             bufferedReader.close()
-
-            System.out.println(lineStr)
-
+            //break encrypted file into 16 byte/128 bit blocks
             val encrypt = lineStr.toByteArray()
+            var encrypt1 = ByteArray(16)
+            var encrypt2 = ByteArray(16)
+            var encrypt3 = ByteArray(16)
+            var encrypt4 = ByteArray(16)
 
-            val iv:ByteArray = ByteArray(16)
-            val decryptedMessage:String = decrypt(iv, testFileKey, encrypt).toString()
+            for(i in 0..15) encrypt1[i] = encrypt[i]
+            for(i in 16..31)encrypt2[i-16] = encrypt[i]
+            for(i in 32..47)encrypt3[i-32] = encrypt[i]
+            for(i in 48..57)encrypt4[i-48] = encrypt[i]
 
-            System.out.println("Decrypted: "+decryptedMessage)
-            //serverOutputTextView.text = decryptedMessage
+            //set initialization vector
+            val iv = ByteArray(16)
+            //AES decrypts at 16 bytes at a given time
+            val decryptedMessage1:String = decrypt(iv, testFileKey, encrypt1).toString()
+            val decryptedMessage2:String = decrypt(iv, testFileKey, encrypt2).toString()
+            val decryptedMessage3:String = decrypt(iv, testFileKey, encrypt3).toString()
+            val decryptedMessage4:String = decrypt(iv, testFileKey, encrypt4).toString()
+
+            serverOutputTextView.text = decryptedMessage1 + decryptedMessage2 + decryptedMessage3 + decryptedMessage4
         }catch (e: IOException)
         {
             e.printStackTrace()
@@ -74,22 +89,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Decrypt AES 128 block
+     * Decrypt AES 128-bits block in 16 bytes blocks
      *
      * test file: testEncryptAES128.jpg
-     * test key: 000102030405060708090A0B0C0D0E0F
+     * test key: 0x000102030405060708090A0B0C0D0E0F
      *
      * @param ivBytes         : initialization vector - 16 bytes for a 128-byte block
-     * @param secretKeyBytes  : secret key
+     * @param secretKeyBytes  : secret key            - 16 bytes
      * @param encrypted       : encrypted block
      * */
     fun decrypt(ivBytes:ByteArray, secretKeyBytes:ByteArray, encrypted:ByteArray):ByteArray
     {
-        var initializationVector:AlgorithmParameterSpec = IvParameterSpec(ivBytes)
-        var secretKeySpec:SecretKeySpec = SecretKeySpec(secretKeyBytes, "AES")
-        var cipher:Cipher = Cipher.getInstance("AES/CBC/NoPadding")
+        val initializationVector = IvParameterSpec(ivBytes)
+        val secretKeySpec = SecretKeySpec(secretKeyBytes, "AES_128")
+        val cipher:Cipher = Cipher.getInstance("AES_128/CBC/NoPadding")
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, initializationVector)
-        return cipher.doFinal(encrypted)
+        try{
+            return cipher.doFinal(encrypted)
+        }catch (e: IllegalBlockSizeException)
+        {
+            var newEncrypted = ByteArray(16)
+            for(i in 0..encrypted.size-1) newEncrypted[i] = encrypted[i]
+            for(i in encrypted.size..15) newEncrypted[i] = 0
+            return cipher.doFinal(newEncrypted)
+        }
     }
 
     fun connectToServer(ipAddress:String, port:Int)
