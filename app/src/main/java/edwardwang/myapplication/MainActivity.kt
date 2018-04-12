2ebffc5a54a899f594cbe83ef09609c81063e85d
Edward Wang <edward.wang.95@gmail.com>
2018-04-12 01:39:09 -0400

    Begin making program modular to handle multiple blocks to decrypt

--- app/src/main/java/edwardwang/myapplication/MainActivity.kt
+++ app/src/main/java/edwardwang/myapplication/MainActivity.kt
@@ -53,67 +53,71 @@ class MainActivity : AppCompatActivity() {
         {
             val serverOutputTextView:TextView = findViewById<TextView>(R.id.serverOutput)
             //break encrypted file into 16 byte/128 bit blocks
-            val inputStream:InputStream = resources.openRawResource(R.raw.secret2)
+            val inputStream:InputStream = resources.openRawResource(R.raw.secret3)
             val readInEncrypt = inputStream.readBytes(16)
-            var encrypt = ByteArray(16)
-            for(i in 0..15) encrypt[i] = readInEncrypt[15-i]
+            System.out.println("Size: "+readInEncrypt.size)
+            //get array of byte array blocks
+            val numOfBlocks:Int = readInEncrypt.size/4
+            var encryptedBlocks:Array<ByteArray> = getEncryptedBlocksByteArray(numOfBlocks,
+                    readInEncrypt)
 
             //get key
-            val testFileKey = ByteArray(16)
-
-            //0c0d0e0f  08090a0b  040506070  0010203
-            /*
-            for(i in 12..15)testFileKey[i-12] = i.toByte()
-            for(i in 8..11) testFileKey[i-4] = i.toByte()
-            for(i in 4..7) testFileKey[i+4] =  i.toByte()
-            for(i in 0..3)testFileKey[i+12] =  i.toByte()
-            */
-
+            val key = ByteArray(16)
             //secret2
             //00000001 00000000 00000000
-            testFileKey[3] = 1.toByte()
+            key[3] = 1.toByte()
 
-
-            System.out.println("Size of encrypt file: "+encrypt.size)
-/*
-            var encrypt1 = ByteArray(16)
-            var encrypt2 = ByteArray(16)
-            var encrypt3 = ByteArray(16)
-            var encrypt4 = ByteArray(16)
-
-            for(i in 0..15) encrypt1[i] = encrypt[i]
-            for(i in 16..31)encrypt2[i-16] = encrypt[i]
-            for(i in 32..47)encrypt3[i-32] = encrypt[i]
-            for(i in 48..57)encrypt4[i-48] = encrypt[i]
-*/
             //set initialization vector
             val iv = ByteArray(16)
+
             //AES decrypts at 16 bytes at a given time
-            val decryptedMessageByteArray = decrypt(iv, testFileKey, encrypt)
-            val correctedDecryptedByteArray = ByteArray(16)
-            for(i in 0..15) correctedDecryptedByteArray[i] = decryptedMessageByteArray[15-i]
-            //val decryptedMessage2:String = decrypt(iv, testFileKey, encrypt2).toString()
-            //val decryptedMessage3:String = decrypt(iv, testFileKey, encrypt3).toString()
-            //val decryptedMessage4:String = decrypt(iv, testFileKey, encrypt4).toString()
+            val decryptedByteArray = getDecryptedByteArray(numOfBlocks, encryptedBlocks, iv, key)
 
-            //serverOutputTextView.text = decryptedMessage1 + decryptedMessage2 + decryptedMessage3 + decryptedMessage4
-
-            //convert byte array to char test
-            /*
-            var decryptedMessage:String? = ""
-            for(i in 0..15)decryptedMessage += decryptedMessageByteArray[i].toChar()
-            serverOutputTextView.text = decryptedMessage
-            */
-
-            ///*
             //convert char to ascii
             val charset = Charsets.US_ASCII
             serverOutputTextView.text = correctedDecryptedByteArray.toString(charset)
-            //*/
         }catch (e: IOException)
         {
             e.printStackTrace()
         }
+    }
+
+    fun getDecryptedByteArray(numOfBlocks: Int,encryptedBlocks:Array<ByteArray>, iv:ByteArray, key:ByteArray):ByteArray
+    {
+        var decryptedByteArray = ByteArray(numOfBlocks)
+        var decryptedBlock = ByteArray(16)
+        for(i in 0..numOfBlocks-1)
+        {
+            for(j in 0..15)
+            {
+                if(j==0) decryptedByteArray[i*16] = decrypt(iv, key, encryptedBlocks[i])
+                else block[j] = byteArray[i*16 + j]
+            }
+        }
+        return decryptedByteArray
+    }
+
+    fun getEncryptedBlocksByteArray(numOfBlocks:Int, byteArray:ByteArray):Array<ByteArray>
+    {
+        var encryptedBlocks = Array<ByteArray>(numOfBlocks,{ ByteArray(16) })
+        for(i in 0..numOfBlocks)
+        {
+            val block = ByteArray(16)
+            for(j in 0..15)
+            {
+                if(j==0) block[j] = byteArray[i * 16]
+                else block[j] = byteArray[i*16 + j]
+            }
+            encryptedBlocks[i] = reverseBlockOrder(block)
+        }
+        return encryptedBlocks
+    }
+
+    fun reverseBlockOrder(byteArray: ByteArray) : ByteArray
+    {
+        var reversedByteArray = ByteArray(16)
+        for(i in 0..15) reversedByteArray[i] = byteArray[15-i]
+        return reversedByteArray
     }
 
     /**
@@ -133,7 +137,7 @@ class MainActivity : AppCompatActivity() {
         val cipher:Cipher = Cipher.getInstance("AES/CBC/NoPadding")
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, initializationVector)
         try{
-            return cipher.doFinal(encrypted)
+            return reverseBlockOrder(cipher.doFinal(encrypted))
         }catch (e: IllegalBlockSizeException)
         {
             System.out.println("Illegal Block Size Exception")
@@ -141,7 +145,7 @@ class MainActivity : AppCompatActivity() {
             var newEncrypted = ByteArray(16)
             for(i in 0..encrypted.size-1) newEncrypted[i] = encrypted[i]
             if(encrypted.size < 15) for(i in encrypted.size..15) newEncrypted[i] = 0
-            return cipher.doFinal(newEncrypted)
+            return reverseBlockOrder(cipher.doFinal(newEncrypted))
         }
     }
 
